4675ec158d90139d44de1b9e225bc29f51f82ba0
Gunturaa <gunturtriatmaja@students.amikom.ac.id>
2023-04-17 19:58:14 +0700

    commit ke-6

---
+++ encapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Mahasiswa mahasiswa1 = new Mahasiswa();
+            mahasiswa1.Nama = "Guntur";
+            mahasiswa1.Nim = "22.11.4820";
+            mahasiswa1.Matakuliah = "Pemrograman";
+            mahasiswa1.Prodi = "S1 Informatika";
+            mahasiswa1.Kampus = "Universitas Amikom Yogyakarta";
+
+            Mahasiswa mahasiswa2 = new Mahasiswa();
+            mahasiswa2.Nama = "Atmaja";
+            mahasiswa2.Nim = "22.11.4820";
+            mahasiswa2.Matakuliah = "Pemrograman";
+            mahasiswa2.Prodi = "S1 Informatika";
+            mahasiswa2.Kampus = "Universitas Amikom Yogyakarta";
+
+            mahasiswa1.info();
+            mahasiswa2.info();
+
+            Console.ReadKey();
+        }
+        
+    }
+}
